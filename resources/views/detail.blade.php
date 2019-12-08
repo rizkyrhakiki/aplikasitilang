ba42860cedded6eb442209fe8f7163dd775ea4a5
rizkyrhakiki <rizkyrhakiki21@gmail.com>
2019-12-09 00:36:44 +0700

    detail

--- resources/views/detail.blade.php
+++ resources/views/detail.blade.php
@@ -4,11 +4,14 @@
     <div class="container-fluid">
         <div class="p-5 w-1/2 rounded overflow-hidden shadow-lg  mx-auto">
             <h1 class="mb-2 text-lg font-bold">{{$informasiTilang['nama']}}</h1>
-            <img class="w-full block pt-5" src="https://tailwindcss.com/img/card-top.jpg"
+            <img class="w-full block pt-5" src="{{asset('storage/'.$informasiTilang['foto'])}}"
                  alt="Sunset in the mountains">
             <div class="px-6 py-4">
-                <p class="text-accent text-sm mb-3">{{$informasiTilang['tanggal_mulai']}}
+                <p class="text-accent text-sm mb-3">Tanggal Tilang : {{$informasiTilang['tanggal_mulai']}}
                     - {{$informasiTilang['tanggal_selesai']}}</p>
+                <p class="text-gray-700 text-base">
+                    Lokasi Tilang : {{$informasiTilang['lokasi']}}
+                </p>
                 <p class="text-gray-700 text-base">
                     {{$informasiTilang['keterangan']}}
                 </p>
